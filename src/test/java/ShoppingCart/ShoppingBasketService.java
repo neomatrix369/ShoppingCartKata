e7ae5d39d26b82eeee2bf4139c02eeda81ea205a
Mani Sarkar <mani@codurance.com>
2016-08-03 19:55:55 +0100

    Removed the Unsupported exception from ShoppingBasketService and replaced it with the simplest code to fetch the basket for a relevant user from a newly created empty hashmap

--- src/test/java/ShoppingCart/ShoppingBasketService.java
+++ src/test/java/ShoppingCart/ShoppingBasketService.java
@@ -1,7 +1,12 @@
 package ShoppingCart;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShoppingBasketService {
+  private Map<UserID, Basket> baskets = new HashMap<>();
+
   public Basket basketFor(UserID userId) {
-    throw new UnsupportedOperationException();
+    return baskets.get(userId);
   }
 }
