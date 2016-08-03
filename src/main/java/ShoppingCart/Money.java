22b32e7db38af46c12b244976d9edd554470e598
Mani Sarkar <mani@codurance.com>
2016-08-03 21:41:39 +0100

    Added equals and hashCode methods to the Money class, in order for the test to pass, last of the classes that needed equals and hashcode

--- src/main/java/ShoppingCart/Money.java
+++ src/main/java/ShoppingCart/Money.java
@@ -1,10 +1,36 @@
 package ShoppingCart;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Money {
   private final double value;
   private final Currency currency;
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Money money = (Money) o;
+
+    return new EqualsBuilder()
+        .append(value, money.value)
+        .append(currency, money.currency)
+        .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+        .append(value)
+        .append(currency)
+        .toHashCode();
+  }
+
   public Money(double value, Currency currency) {
+
     this.value = value;
     this.currency = currency;
   }
