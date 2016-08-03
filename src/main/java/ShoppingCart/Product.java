1342bf2a039bfaeee7849e326472ebe1aa621ff2
Mani Sarkar <mani@codurance.com>
2016-08-03 21:34:44 +0100

    Added equals and hashCode methods to the Product class

--- src/main/java/ShoppingCart/Product.java
+++ src/main/java/ShoppingCart/Product.java
@@ -1,5 +1,8 @@
 package ShoppingCart;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Product {
   private final ProductID productID;
   private final String title;
@@ -9,5 +12,29 @@ public class Product {
     this.productID = productID;
     this.title = title;
     this.price = price;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Product product = (Product) o;
+
+    return new EqualsBuilder()
+        .append(productID, product.productID)
+        .append(title, product.title)
+        .append(price, product.price)
+        .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+        .append(productID)
+        .append(title)
+        .append(price)
+        .toHashCode();
   }
 }
