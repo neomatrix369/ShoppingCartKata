8a10c8cb3108f170315901cbe0f921513d4c14fb
Mani Sarkar <mani@codurance.com>
2016-08-03 20:29:13 +0100

    Added equals, hashCode, and toStrings method to the Basket class

--- src/test/java/ShoppingCart/Basket.java
+++ src/test/java/ShoppingCart/Basket.java
@@ -3,6 +3,9 @@ package ShoppingCart;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Basket {
   private final List<BasketItem> items;
   private final LocalDate date;
@@ -10,5 +13,33 @@ public class Basket {
   public Basket(List<BasketItem> items, LocalDate date) {
     this.items = items;
     this.date = date;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o) return true;
+
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Basket basket = (Basket) o;
+
+    return new EqualsBuilder()
+        .append(items, basket.items)
+        .append(date, basket.date)
+        .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+        .append(items)
+        .append(date)
+        .toHashCode();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Basket{items=%s, date=%s}", items, date);
   }
 }
