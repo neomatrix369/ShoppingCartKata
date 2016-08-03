336772de1b2178d9cab45d2bf53614c67b3d5ad1
Mani Sarkar <mani@codurance.com>
2016-08-03 19:53:45 +0100

    Removed the Unsupported exception from Basket and bound the parameters with private fields.

--- src/test/java/ShoppingCart/Basket.java
+++ src/test/java/ShoppingCart/Basket.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Basket {
+  private final List<BasketItem> items;
+  private final LocalDate date;
+
   public Basket(List<BasketItem> items, LocalDate date) {
-    throw new UnsupportedOperationException();
+    this.items = items;
+    this.date = date;
   }
 }
