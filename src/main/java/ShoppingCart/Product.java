efb7223f2a093046e022aaa5da29c85ee92261de
Mani Sarkar <mani@codurance.com>
2016-08-03 21:33:52 +0100

    Removed the unsupported exception from the Product constructor with parameters and fields bindings

--- src/main/java/ShoppingCart/Product.java
+++ src/main/java/ShoppingCart/Product.java
@@ -1,7 +1,13 @@
 package ShoppingCart;
 
 public class Product {
+  private final ProductID productID;
+  private final String title;
+  private final Money price;
+
   public Product(ProductID productID, String title, Money price) {
-    throw new UnsupportedOperationException();
+    this.productID = productID;
+    this.title = title;
+    this.price = price;
   }
 }
