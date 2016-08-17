32910a7edd5a1ca3b39407127bc002812f872171
Mani Sarkar <mani@codurance.com>
2016-08-18 00:35:05 +0100

    Refactored and simplified tests to make it easier to read and understand.

--- src/test/java/ShoppingCart/domain/BasketShould.java
+++ src/test/java/ShoppingCart/domain/BasketShould.java
@@ -4,20 +4,32 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ShoppingCart.infrastructure.Clock;
 import ShoppingCart.infrastructure.ProductRepository;
 
 public class BasketShould {
+
+  private static final ProductID PRODUCT_ID_100001 = new ProductID(100001);
+  private static final GBP TWENTY_POUNDS = new GBP(20);
+
+  private ProductRepository productRepository;
+  private Clock clock;
+  private Basket basket;
+
+  @Before
+  public void initialise() {
+    productRepository = new ProductRepository();
+    clock = new Clock();
+    basket = new Basket(clock.getCurrentDate(), productRepository);
+  }
+
   @Test public void
   calculate_the_total_when_items_are_added_to_it() {
-    ProductRepository productRepository = new ProductRepository();
-    Clock clock = new Clock();
-    Basket basket = new Basket(clock.getCurrentDate(), productRepository);
+    basket = basket.addItem(new BasketItem(PRODUCT_ID_100001, 2));
 
-    basket = basket.addItem(new BasketItem(new ProductID(100001), 2));
-
-    assertThat(basket.getTotal(), is(equalTo(new GBP(20))));
+    assertThat(basket.getTotal(), is(equalTo(TWENTY_POUNDS)));
   }
 }
