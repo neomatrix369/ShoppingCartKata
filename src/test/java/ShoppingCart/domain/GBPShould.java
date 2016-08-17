a23a211ebe4cc64048f03542b8b6dc36efaecfdf
Mani Sarkar <mani@codurance.com>
2016-08-18 00:54:07 +0100

    Added the second and final test for GBP in GBPShould

--- src/test/java/ShoppingCart/domain/GBPShould.java
+++ src/test/java/ShoppingCart/domain/GBPShould.java
@@ -9,9 +9,16 @@ public class GBPShould {
 
   private static final GBP FIVE_POUNDS = new GBP(5.00);
   private static final GBP FIFTY_POUNDS = new GBP(50.00);
+  private static final GBP TEN_POUNDS = new GBP(10.00);
+  private static final GBP FIFTEEN_POUNDS = new GBP(15.00);
 
   @Test public void
-  return_a_multiple_of_the_original_value() {
+  return_a_multiple_of_the_original_value_by_the_multiplyBy() {
     assertThat(FIVE_POUNDS.multiplyBy(10), is(FIFTY_POUNDS));
+  }
+
+  @Test public void
+  return_a_total_value_of_the_original_value_in_the_tests() {
+    assertThat(FIVE_POUNDS.plus(TEN_POUNDS), is(FIFTEEN_POUNDS));
   }
 }
