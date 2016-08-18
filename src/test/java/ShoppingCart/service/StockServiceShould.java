40587d1a31b323b9fce69e7a7aaec21d873503d5
Mani Sarkar <mani@codurance.com>
2016-08-19 00:29:56 +0100

    Refactored test class by removing duplications between the test cases

--- src/test/java/ShoppingCart/service/StockServiceShould.java
+++ src/test/java/ShoppingCart/service/StockServiceShould.java
@@ -1,35 +1,38 @@
 package ShoppingCart.service;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ShoppingCart.domain.OutOfStockException;
 import ShoppingCart.domain.ProductID;
 
 public class StockServiceShould {
+
+  private static final ProductID ANY_PRODUCT = new ProductID(100001);
+  private StockService stockService;
+
+  @Before
+  public void initialise() {
+    stockService = new StockService();
+  }
+
   @Test (expected = OutOfStockException.class) public void
   throw_an_exception_when_deducting_quantity_for_an_item_that_does_not_have_any_stock() throws OutOfStockException {
-    ProductID productId = new ProductID(100001);
-    StockService stockService = new StockService();
-
-    stockService.deductStockOrThrowError(productId, 1);
+    stockService.deductStockOrThrowError(ANY_PRODUCT, 1);
   }
 
   @Test (expected = OutOfStockException.class) public void
   throw_an_exception_when_deducting_quantity_for_an_item_with_not_enough_stock() throws OutOfStockException {
-    ProductID productId = new ProductID(100001);
-    StockService stockService = new StockService();
-    stockService.updateStock(productId, 2);
+    stockService.updateStock(ANY_PRODUCT, 2);
 
-    stockService.deductStockOrThrowError(productId, 3);
+    stockService.deductStockOrThrowError(ANY_PRODUCT, 3);
   }
 
   @Test public void
   throw_no_exception_when_deducting_quantity_for_an_item_with_enough_stock() throws OutOfStockException {
-    ProductID productId = new ProductID(100001);
-    StockService stockService = new StockService();
-    stockService.updateStock(productId, 3);
+    stockService.updateStock(ANY_PRODUCT, 3);
 
-    stockService.deductStockOrThrowError(productId, 2);
-    stockService.deductStockOrThrowError(productId, 1);
+    stockService.deductStockOrThrowError(ANY_PRODUCT, 2);
+    stockService.deductStockOrThrowError(ANY_PRODUCT, 1);
   }
 }
