985d59e68926a5c17b10baa3c9d2f78648965635
Mani Sarkar <mani@codurance.com>
2016-08-19 00:26:47 +0100

    Added third test to StockService to show that no exception is thrown when there is enough stock, and an attempt is made to deduct stock

--- src/test/java/ShoppingCart/service/StockServiceShould.java
+++ src/test/java/ShoppingCart/service/StockServiceShould.java
@@ -7,7 +7,7 @@ import ShoppingCart.domain.ProductID;
 
 public class StockServiceShould {
   @Test (expected = OutOfStockException.class) public void
-  throws_an_exception_when_deducting_quantity_for_an_item_that_does_not_have_any_stock() throws OutOfStockException {
+  throw_an_exception_when_deducting_quantity_for_an_item_that_does_not_have_any_stock() throws OutOfStockException {
     ProductID productId = new ProductID(100001);
     StockService stockService = new StockService();
 
@@ -15,11 +15,21 @@ public class StockServiceShould {
   }
 
   @Test (expected = OutOfStockException.class) public void
-  throws_an_exception_when_deducting_quantity_for_an_item_with_not_enough_stock() throws OutOfStockException {
+  throw_an_exception_when_deducting_quantity_for_an_item_with_not_enough_stock() throws OutOfStockException {
     ProductID productId = new ProductID(100001);
     StockService stockService = new StockService();
     stockService.updateStock(productId, 2);
 
     stockService.deductStockOrThrowError(productId, 3);
-  } 
+  }
+
+  @Test public void
+  throw_no_exception_when_deducting_quantity_for_an_item_with_enough_stock() throws OutOfStockException {
+    ProductID productId = new ProductID(100001);
+    StockService stockService = new StockService();
+    stockService.updateStock(productId, 3);
+
+    stockService.deductStockOrThrowError(productId, 2);
+    stockService.deductStockOrThrowError(productId, 1);
+  }
 }
