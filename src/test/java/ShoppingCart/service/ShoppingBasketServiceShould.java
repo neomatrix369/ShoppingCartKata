5e74a5e952d618ea683e84fc866c2fe07e00a950
Mani Sarkar <mani@codurance.com>
2016-08-30 10:49:13 +0100

    Replaced state based testing for StockService with mocks -  resolved two TODO items

--- src/test/java/ShoppingCart/service/ShoppingBasketServiceShould.java
+++ src/test/java/ShoppingCart/service/ShoppingBasketServiceShould.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static java.lang.String.format;
@@ -12,7 +13,6 @@ import static java.lang.String.format;
 import org.junit.Before;
 import org.junit.Test;
 
-import ShoppingCart.infrastructure.Console;
 import ShoppingCart.domain.Basket;
 import ShoppingCart.domain.BasketItem;
 import ShoppingCart.domain.OutOfStockException;
@@ -20,6 +20,7 @@ import ShoppingCart.domain.ProductID;
 import ShoppingCart.domain.UserID;
 import ShoppingCart.infrastructure.BasketRepository;
 import ShoppingCart.infrastructure.Clock;
+import ShoppingCart.infrastructure.Console;
 import ShoppingCart.infrastructure.ProductRepository;
 
 public class ShoppingBasketServiceShould {
@@ -44,15 +45,12 @@ public class ShoppingBasketServiceShould {
     console = mock(Console.class);
     basketRepository = new BasketRepository();
     productRepository = new ProductRepository();
-    stockService = new StockService();
+    stockService = mock(StockService.class);
     shoppingBasketService =
         new ShoppingBasketService(console, clock, basketRepository, productRepository, stockService);
     userOne = new UserID(1);
     userTwo = new UserID(2);
     userThree = new UserID(3);
-
-    stockService.updateStock(BOOK_THE_HOBBIT, 5);
-    stockService.updateStock(DVD_BREAKING_BAD, 10);
   }
 
   @Test public void
@@ -137,17 +135,18 @@ public class ShoppingBasketServiceShould {
             clock.getCurrentDate(), userOne, BOOK_THE_HOBBIT, 3, productRepository.getProductBy(BOOK_THE_HOBBIT).getPrice()));
   }
 
-  //TODO Check for flow by adding mocks to stockService
   @Test (expected = OutOfStockException.class) public void
   throw_an_exception_if_ordered_item_is_not_in_stock() throws OutOfStockException {
-    stockService.updateStock(DVD_BREAKING_BAD, 0);
+    doThrow(OutOfStockException.class).when(stockService).reserveStock(DVD_BREAKING_BAD, 2);
+
     shoppingBasketService.addItem(userOne, DVD_BREAKING_BAD, 2);
   }
 
-  //TODO Check for flow by adding mocks to stockService
   @Test (expected = OutOfStockException.class) public void
   deduct_the_items_from_the_stock_when_added_to_the_basket_and_throw_an_exception_if_quantity_exceeds_stock_count()
       throws OutOfStockException {
+    doThrow(OutOfStockException.class).when(stockService).reserveStock(BOOK_THE_HOBBIT, 1);
+
     shoppingBasketService.addItem(userOne, BOOK_THE_HOBBIT, 2);
     shoppingBasketService.addItem(userTwo, BOOK_THE_HOBBIT, 3);
     shoppingBasketService.addItem(userThree, BOOK_THE_HOBBIT, 1);
